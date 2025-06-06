cc7da04626bfb82b2ed368fc2af05b98c15de7d8
BeepBoopManifesto <kevrut9@gmail.com>
2025-06-06 13:23:06 -0400

    added get character number script to stats and imported to main

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from stats import get_num_words
+from stats import get_num_characters
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_charaters = get_num_characters(text)
     print(f"{num_words} words found in the document")
+    print(num_charaters)
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_characters(text):
+    lowercase = text.lower()
+    characters_num = {}
+    for character in lowercase:
+        if character in characters_num:
+            characters_num[character] += 1
+        else:
+            characters_num[character] = 1
+    return characters_num
