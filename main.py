58cd75f093eb2be8109f543421442dd8fb11b9fc
BeepBoopManifesto <kevrut9@gmail.com>
2025-06-10 18:25:01 -0400

    change book_path variable to be handeled by sys.argv

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from stats import get_num_words
 from stats import get_num_characters
 from stats import chars_dict_to_sorted_list
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    characters_dict = get_num_characters(text)
-    chars_sorted_list = chars_dict_to_sorted_list(characters_dict)
-    print_report(book_path, num_words, chars_sorted_list)
-
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        characters_dict = get_num_characters(text)
+        chars_sorted_list = chars_dict_to_sorted_list(characters_dict)
+        print_report(book_path, num_words, chars_sorted_list)
+    
 
 def get_book_text(path):
     with open(path) as f:
